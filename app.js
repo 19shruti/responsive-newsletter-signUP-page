455a68cd9e609af3f4d612bc1acc2120913dcce8
SHRUTI RAJ <69633187+19shruti@users.noreply.github.com>
2021-08-13 06:24:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -33,28 +33,28 @@ app.post("/",function(req,res){
     };
     const jsonData=JSON.stringify(data);
 
-const url="https://us5.api.mailchimp.com/3.0/lists/e6b15d34de";
-const options={
-    method:"Post",
-    auth:"shrutiRaj:764a3090b413da0b8c4af43cdb647773-us5"
-}
-const request=https.request(url,options,function(response){
+// const url="https://us5.api.mailchimp.com/3.0/lists/e6b15d34de";
+// const options={
+//     method:"Post",
+//     auth:"shrutiRaj:764a3090b413da0b8c4af43cdb647773-us5"
+// }
+// const request=https.request(url,options,function(response){
 
-  if(  response.statusCode === 200)
-  {
-      res.sendFile(__dirname+"/success.html")
-  }
-  else{
-      res.sendFile(__dirname+"/failure.html")
-  }
+//   if(  response.statusCode === 200)
+//   {
+//       res.sendFile(__dirname+"/success.html")
+//   }
+//   else{
+//       res.sendFile(__dirname+"/failure.html")
+//   }
 
-response.on("data",function(data){
-    console.log(JSON.parse(data));
-})
-})
-request.write(jsonData);
-request.end();
-});
+// response.on("data",function(data){
+//     console.log(JSON.parse(data));
+// })
+// })
+// request.write(jsonData);
+// request.end();
+// });
 
 
 app.post("/failure",function(req,res){
